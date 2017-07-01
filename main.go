93d7e92c9e64375e118d6d546b4d2926cfb9302b
blobdon <zachj1@gmail.com>
2017-07-01 22:50:26 +0100

    add page handler

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
+// data for templates
+type d map[string]interface{}
+
 // Handler for home page
+// /user/:id, i.e. /user/ABC1234567
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("static/dashboard.html")
 	if err != nil {
@@ -14,8 +19,34 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, struct{}{})
 }
 
+// Handler for user page
+// /user/:id, i.e. /user/ABC1234567
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	page := r.URL.Path
+	fmt.Println("page", page)
+	t, err := template.ParseFiles("static/" + page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Execute(w, struct{}{})
+}
+
+// hangdler for questions
+// func questionsHandler(w http.ResponseWriter, r *http.Request) {
+
+// 	if r.Method == http.MethodGet {
+
+// 	} else if r.Method == http.MethodPost {
+// 		http.Redirect(w, r, "/u/")
+// 	} else {
+// 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+// 	}
+
+// }
+
 func main() {
-	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/", pageHandler)
+	http.HandleFunc("/user/", mainHandler)
 	http.Handle("/assets/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8080", nil)
 }
